3cc5b52ac1b2c9d777b5382660bcc44c067988df
Roy Yeh <royyeh007@gmail.com>
2018-10-30 16:23:17 -0500

    basic css(require npm i) footer, navbar, basic main page setting

---
+++ client/components/footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="footers">
+      <a> Follow Us </a>
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+      />
+      <a href="#" className="fa fa-facebook" />
+      <a href="#" className="fa fa-twitter" />
+      <a href="#" className="fa fa-instagram" />
+      <a href="#" className="fa fa-youtube" />
+    </div>
+  )
+}
+export default Footer
