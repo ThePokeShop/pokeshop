1309f120b887c0ddf77f6f9a4ce0aee3966e87e2
Dilan Ozusta <dilanozusta@outlook.com>
2018-10-31 23:55:26 -0500

    models for lineItem and Order created

---
+++ server/db/models/lineItem.js
@@ -0,0 +1,21 @@
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+
+const LineItem = db.define('lineItem', {
+  totalPrice: {
+    type: Sequelize.DECIMAL(10, 2),
+    allowNull: false,
+    validate: {
+      min: 0,
+    }
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 0
+    }
+  }
+})
+module.exports = LineItem;
