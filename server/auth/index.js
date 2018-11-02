d5fb47f034b2e02928ecade9394bed65d93824e8
David Jung <djung31@gmail.com>
2018-11-02 15:57:46 -0500

    POST auth/signup sends email, GET auth/confirmation confirms token, GET auth/resend/:userId sends new token, change User model, created EmailToken model

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,6 +1,12 @@
 const router = require('express').Router()
 const User = require('../db/models/user')
+const nodemailer = require('nodemailer')
+const crypto = require('crypto')
+const EmailToken = require('../db/models/emailToken')
+const Sequelize = require('sequelize')
 module.exports = router
+
+// defines URL for testing/production
 
 router.post('/login', async (req, res, next) => {
   try {
@@ -11,6 +17,9 @@ router.post('/login', async (req, res, next) => {
     } else if (!user.correctPassword(req.body.password)) {
       console.log('Incorrect password for user:', req.body.email)
       res.status(401).send('Wrong username and/or password')
+    } else if (!user.isEmailVerified) {
+      console.log('Email is not verified for user: ', req.body.email)
+      res.status(401).send('Email has not been verified yet')
     } else {
       req.login(user, err => (err ? next(err) : res.json(user)))
     }
@@ -19,16 +28,55 @@ router.post('/login', async (req, res, next) => {
   }
 })
 
+// creates a token and sends a link containing token to email address
 router.post('/signup', async (req, res, next) => {
+  let url =
+    process.env.NODE_ENV === 'development'
+      ? 'http://localhost:8080'
+      : 'https://the-poke-shop.herokuapp.com'
+  let user
   try {
-    const user = await User.create(req.body)
-    req.login(user, err => (err ? next(err) : res.json(user)))
+    user = await User.create(req.body)
   } catch (err) {
     if (err.name === 'SequelizeUniqueConstraintError') {
       res.status(401).send('User already exists')
     } else {
       next(err)
     }
+  }
+  try {
+    const token = await EmailToken.create({
+      userId: user.id,
+      emailToken: crypto.randomBytes(16).toString('hex')
+    })
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL_ADDRESS,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    })
+    const mailOptions = {
+      from: 'no-reply@the-poke-shop.herokuapp.com',
+      to: user.email,
+      subject: 'Account Verification Token',
+      html: `<p>Hello, ${
+        user.name
+      }!</p><p>Please verify your account by clicking the link:</p><a href="${url}/auth/confirmation/${
+        token.emailToken
+      }">${url}/auth/confirmation/${
+        token.emailToken
+      }</a><p>This link will expire in twelve hours.</p>`
+    }
+    transporter.sendMail(mailOptions, err => {
+      if (err) return res.status(500).send({msg: err.message})
+      res
+        .status(200)
+        .send('A verification email has been sent to ' + user.email + '.')
+    })
+  } catch (err) {
+    console.error(err);
+    next(err)
   }
 })
 
@@ -43,3 +91,85 @@ router.get('/me', (req, res) => {
 })
 
 router.use('/google', require('./google'))
+
+router.get('/confirmation/:token', async (req, res, next) => {
+  try {
+    const token = req.params.token
+    const emailToken = await EmailToken.findOne({where: {emailToken: token}})
+    if (emailToken === null) return res.status(400).send('Token not found.')
+
+    const user = await User.findById(emailToken.userId)
+    // token has already been checked
+    if (emailToken.wasVerified)
+      return res.status(400).send('Token already used')
+    if (emailToken.wasVerified === false)
+      return res.status(400).send('Token already expired.')
+    if (!user)
+      return res.status(400).send('Unable to find a user for this token')
+
+    // token hasn't been checked yet
+    const timeDiff = new Date() - emailToken.updatedAt
+    if (timeDiff / 3600000 <= 12) {
+      await Promise.all([
+        user.update({isEmailVerified: true}),
+        emailToken.update({wasVerified: true})
+      ])
+      return req.login(user, err => (err ? next(err) : res.json(user)))
+    } else {
+      await emailToken.update({wasVerified: false})
+      return res.status(400).send('Token expired.')
+    }
+  } catch (err) {
+    console.error(err)
+    next(err)
+  }
+})
+
+router.get('/resend/:userId', async (req, res, next) => {
+  const userId = req.params.userId
+  try {
+    const user = await User.findById(userId)
+    if (user === null) return res.status(400).send('User not found')
+    if (user.isEmailVerified)
+      return res.status(400).send('User already verified')
+
+    const token = await EmailToken.create({
+      userId: userId,
+      emailToken: crypto.randomBytes(16).toString('hex')
+    })
+    const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+        user: process.env.EMAIL_ADDRESS,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    })
+
+    const url =
+      process.env.NODE_ENV === 'development'
+        ? 'http://localhost:8080'
+        : 'https://the-poke-shop.herokuapp.com'
+
+    const mailOptions = {
+      from: 'no-reply@the-poke-shop.herokuapp.com',
+      to: user.email,
+      subject: 'Account Verification Token',
+      html: `<p>Hello, ${
+        user.name
+      }!</p><p>Please verify your account by clicking the link:</p><a href="${url}/auth/confirmation/${
+        token.emailToken
+      }">${url}/auth/confirmation/${
+        token.emailToken
+      }</a><p>This link will expire in twelve hours.</p>`
+    }
+    transporter.sendMail(mailOptions, err => {
+      if (err) return res.status(500).send({msg: err.message})
+      res
+        .status(200)
+        .send('A verification email has been sent to ' + user.email + '.')
+    })
+  } catch (err) {
+    console.error(err)
+    next(err);
+  }
+})
--- server/db/models/index.js
+++ server/db/models/index.js
@@ -3,7 +3,7 @@ const Product = require('./product')
 const Category = require('./category');
 const LineItem = require('./lineItem');
 const Order = require('./order');
-
+const EmailToken = require('./emailToken');
 /**
  * If we had any associations to make, this would be a great place to put them!
  * ex. if we had another model called BlogPost, we might say:
@@ -13,10 +13,13 @@ const Order = require('./order');
 
 Product.belongsToMany(Category, { through: 'CategoryProducts', as: 'Category'});
 Category.belongsToMany(Product, { through: 'CategoryProducts' });
+
 LineItem.belongsTo(Product);
 Order.hasMany(LineItem);
-Order.hasOne(User);
+Order.belongsTo(User);
 
+EmailToken.belongsTo(User); // each email token has a userId key
+User.hasMany(EmailToken);  // not sure if has many
 /**
  * We'll export all of our models here, so that any time a module needs a model,
  * we can just require it from 'db/models'
@@ -24,5 +27,5 @@ Order.hasOne(User);
  * instead of: const User = require('../db/models/user')
  */
 module.exports = {
-  User, Product, Category, LineItem, Order
+  User, Product, Category, LineItem, Order, EmailToken
 }
